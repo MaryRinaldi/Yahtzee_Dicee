f6d7787bb09d13349cebb917e1930c25621fdfa6
Maria-Carolina-Rinaldi <dr.rinaldimaria@gmail.com>
2024-02-23 12:40:24 +0100

    Initial EXpress commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,11 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
