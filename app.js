7df28e7284864a05ea70ed5739a3a4e4345ef84c
Maria-Carolina-Rinaldi <dr.rinaldimaria@gmail.com>
2024-02-26 22:11:06 +0100

    first part- front

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
+const dice = document.querySelecton('dice')
+const rollBtn = document.querySelector('.roll')
 
 app.use(cors());
 app.use(logger('dev'));
@@ -16,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
+
 
 module.exports = app;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+export default function App() {
+const [rollBtn, setRollBtn] = useState("");
+
+const randomDice = () => {
+  const random = Math.floor(Math.random() * 10);
+  if (random >= 1 && random <= 6) {
+    rollDice(random)
+  } else {
+    randomDice()
+  }
+}  
+
+const rollDice = random => {
+  dice.style.animation = 'rolling 4s';
+  setTimeout(() => {
+    switch (random) {
+      case 1:
+        dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+        break;
+      case 6:
+        dice.style.transform = 'rotateX(180deg) rotateY(0deg)';
+        break;
+      case 2:
+        dice.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+        break;
+      case 5:
+        dice.style.transform = 'rotateX(90deg) rotateY(0deg)';
+        break;
+      case 3:
+        dice.style.transform = 'rotateX(0deg) rotateY(90deg)';
+        break;
+      case 4:
+        dice.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+        break;
+      default:
+        break;
+    }
+    dice.style.animation = "none";
+  }, 4050);
 }
 
-export default App
+
+
+    
+
+
+
+  return (
+    <body>
+    <title>Yahtzee Dice</title>
+     <div className="container">
+      <h2>ROLL DICE</h2>
+       <div className="dice">
+        <div className="face front"></div>
+        <div className="face back"></div>
+        <div className="face top"></div>
+        <div className="face bottom"></div>
+        <div className="face right"></div>
+        <div className="face left"></div>        
+      </div>
+      <button className='roll' onClick={rollDice} >
+        Roll
+      </button>
+    </div>
+
+    </body>
+   
+  );
+};
